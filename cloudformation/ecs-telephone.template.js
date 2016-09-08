93d6f6b96a65b9fe04e35c61470858491d0a668a
Ryan Clark <ryan.clark.j@gmail.com>
2016-09-07 17:16:11 -0700

    experimental watchbot

--- cloudformation/ecs-telephone.template.js
+++ cloudformation/ecs-telephone.template.js
@@ -10,6 +10,7 @@ const watcher = watchbot.template({
   cluster: cf.ref('Cluster'),
   service: 'ecs-telephone',
   serviceVersion: cf.ref('GitSha'),
+  watchbotVersion: '7b0352449c74ba5ef3054f9c56d671cc5ea0dde3',
   workers: 1,
   reservation: { cpu: 256, memory: 128 },
   env: { StackRegion: cf.region },
