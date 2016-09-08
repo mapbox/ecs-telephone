cff4e9f40cfcb86fa27d3057950d0e06b7684266
Ryan Clark <ryan.clark.j@gmail.com>
2016-09-07 18:22:10 -0700

    watchbump

--- cloudformation/ecs-telephone.template.js
+++ cloudformation/ecs-telephone.template.js
@@ -10,7 +10,7 @@ const watcher = watchbot.template({
   cluster: cf.ref('Cluster'),
   service: 'ecs-telephone',
   serviceVersion: cf.ref('GitSha'),
-  watchbotVersion: '7b0352449c74ba5ef3054f9c56d671cc5ea0dde3',
+  watchbotVersion: '87fc503d963c4769b76d36988c361c62e9d03984',
   workers: 1,
   reservation: { cpu: 256, memory: 128 },
   env: { StackRegion: cf.region },
