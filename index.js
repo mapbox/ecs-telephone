2dafde280adf8eba86371e793c10feddb322672e
Ryan Clark <ryan.clark.j@gmail.com>
2017-02-07 16:49:29 -0700

    one message at a time

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 
 const watchbot = require('@mapbox/watchbot');
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
 const random = require('random-words');
 
-const region = process.env.StackRegion;
-const TopicArn = process.env.WorkTopic;
+// const region = process.env.StackRegion;
+// const TopicArn = process.env.WorkTopic;
 const Subject = process.env.Subject;
 const message = process.env.Message;
-const sns = new AWS.SNS({ region });
+// const sns = new AWS.SNS({ region });
 
 const words = message.split(' ');
 words[Math.floor(Math.random() * (words.length + 1))] = random(1)[0];
 const Message = words.join(' ');
 
-const publish = sns.publish({ TopicArn, Subject, Message });
+// const publish = sns.publish({ TopicArn, Subject, Message });
 
-publish.promise()
+// publish.promise()
+Promise.resolve()
   .then(() => watchbot.log(`${Subject}: ${Message}`))
   .catch(err => watchbot.log(err));
