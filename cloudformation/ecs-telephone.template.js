28d2c7b03b8224bb3df6fb3bff5a8585467c8484
jakepruitt <jake@mapbox.com>
2017-02-08 13:52:05 -0800

    Nevermind you can use your version of watchbot i'll be over here

--- cloudformation/ecs-telephone.template.js
+++ cloudformation/ecs-telephone.template.js
@@ -10,7 +10,6 @@ const watcher = watchbot.template({
   cluster: cf.ref('Cluster'),
   service: 'ecs-telephone',
   serviceVersion: cf.ref('GitSha'),
-  watchbotVersion: 'ac868ff3360e794c7a8edd403bd7dcb62a82e1e2',
   workers: 1,
   reservation: { cpu: 256, memory: 128 },
   env: { StackRegion: cf.region },
