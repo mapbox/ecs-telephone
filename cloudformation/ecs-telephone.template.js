989cb5099bfd059638020e8c30e1814e69220f7d
Ryan Clark <ryan.clark.j@gmail.com>
2017-09-18 15:16:03 -0700

    no categorizer

--- cloudformation/ecs-telephone.template.js
+++ cloudformation/ecs-telephone.template.js
@@ -1,18 +1,16 @@
 const watchbot = require('@mapbox/watchbot');
 const cf = require('@mapbox/cloudfriend');
-const categorizer = require('@mapbox/categorizer');
 
 const Parameters = {
   GitSha: { Type: 'String' },
   Cluster: { Type: 'String' },
-  Team: categorizer.team('platform'),
-  CostCategory: categorizer.category(['rd'])
+  Family: { Type: 'String' }
 };
 
 const watcher = watchbot.template({
   cluster: cf.ref('Cluster'),
   service: 'ecs-telephone',
-  family: categorizer.family(),
+  family: cf.ref('Family'),
   serviceVersion: cf.ref('GitSha'),
   workers: 1,
   reservation: { cpu: 256, memory: 128 },
