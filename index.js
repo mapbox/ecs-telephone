3a8b47f35b50b9c70be200b853643b066bab1a7d
jakepruitt <jake@mapbox.com>
2018-05-23 17:56:10 -0700

    Watchbot logger

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const TopicArn = process.env.WorkTopic;
 const Subject = process.env.Subject;
 const message = process.env.Message;
 const sns = new AWS.SNS({ region });
+const logger = new watchbot.Logger('worker');
 
 const fs = require('fs');
 
@@ -16,16 +17,16 @@ const words = message.split(' ');
 words[Math.floor(Math.random() * (words.length + 1))] = random(1)[0];
 const Message = words.join(' ');
 
-watchbot.log('Let\'s write to the /tmp directory');
+logger.log('Let\'s write to the /tmp directory');
 
 fs.write('/tmp/file.txt', 'can i write here?');
 
-watchbot.log('Let\'s write to the /mnt directory');
+logger.log('Let\'s write to the /mnt directory');
 
 fs.write('/mnt/file.txt', 'can i write here?');
 
 const publish = sns.publish({ TopicArn, Subject, Message });
 
 publish.promise()
-  .then(() => watchbot.log(`${Subject}: ${Message}`))
-  .catch(err => watchbot.log(err));
+  .then(() => logger.log(`${Subject}: ${Message}`))
+  .catch(err => logger.log(err));
