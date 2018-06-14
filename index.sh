322c97a7b85793c9b1e302da31d66f9683ac2bd1
arunasank <aruna@mapbox.com>
2018-06-14 06:13:09 +0530

    self sustaining

--- index.sh
+++ index.sh
@@ -3,3 +3,4 @@
 sleepTime=$(echo $((1 + RANDOM % 5)))
 echo "I enjoy a good sleep for $sleepTime"
 sleep $sleepTime
+aws sns publish --topic-arn "$WorkTopic" --subject "$Subject" --message "I enjoy a good sleep" --region "$StackRegion"
