3189ddeec3199e9291b22b334d7e6e62cda7bc50
Daniel Patterson <daniel@mapbox.com>
2018-08-29 05:54:12 -0700

    Replace words using rhyming dictionary instead of random replacement (#14)
    * Replace words using rhyming dictionary instead of random replacement, to keep with the true spirit of the Telephone game.
    
    * Update README with example of rhyming replacement.
    
    * Fix typo.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const AWS = require('aws-sdk');
-const random = require('random-words');
+const rhyme = require('rhyme');
 
 const region = process.env.StackRegion;
 const TopicArn = process.env.WorkTopic;
@@ -9,12 +9,22 @@ const Subject = process.env.Subject;
 const message = process.env.Message;
 const sns = new AWS.SNS({ region });
 
-const words = message.split(' ');
-words[Math.floor(Math.random() * (words.length + 1))] = random(1)[0];
-const Message = words.join(' ');
 
-const publish = sns.publish({ TopicArn, Subject, Message });
+rhyme((rhymer) => {
+  const words = message.split(' ');
+  const wordToChange = Math.floor(Math.random() * words.length);
+  const probabilityOfChange = 0.75;
+  if (Math.random() <= probabilityOfChange) {
+    const rhymes = rhymer.rhyme(words[wordToChange]);
+    if (rhymes.length > 0) { // only replace if there are rhymes for this word
+      words[wordToChange] = rhymes[Math.floor(Math.random() * rhymes.length)];
+    }
+  }
+  const Message = words.join(' ');
 
-publish.promise()
-  .then(() => console.log(`${Subject}: ${Message}`))
-  .catch(err => console.log(err));
+  const publish = sns.publish({ TopicArn, Subject, Message });
+
+  publish.promise()
+    .then(() => console.log(`${Subject}: ${Message}`))
+    .catch(err => console.log(err));
+});
