73a3551b37a1bfeef87cee675d94055240c601b1
Kushan Joshi <0o3ko0@gmail.com>
2020-02-13 15:12:04 -0800

    Try

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const AWS = require('aws-sdk');
-const rhyme = require('rhyme');
+const AWS = require("aws-sdk");
+const rhyme = require("rhyme");
 
 const region = process.env.StackRegion;
 const TopicArn = process.env.WorkTopic;
@@ -9,3 +9,4 @@ const Subject = process.env.Subject;
 const message = process.env.Message;
 const sns = new AWS.SNS({ region });
 
+const file = require("./file");
